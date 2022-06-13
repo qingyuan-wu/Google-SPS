23ba1a6966eca725c3ee0722933da1c325ed051c
Qingyuan Wu <qyw.wu@mail.utoronto.ca>
2022-06-13 14:41:31 +0000

    form submit render html

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -1,6 +1,8 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -21,6 +23,20 @@ public class FormHandlerServlet extends HttpServlet {
     System.out.println("You submitted: " + email);
 
     // Write the value to the response so the user can see it.
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+    
     response.getWriter().println("Your name: " + name + ". Your email: " + email);
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<title>Your submission</title>");
+    out.println("</head>");
+    out.println("<body bgcolor=\"white\">");
+    out.println("<div>");
+    out.println("Your name: " + name + ". Your email: " + email);
+    out.println("</div>");
+    out.println("<a href=\"/\">Back</a>");
+    out.println("</body>");
+    out.println("</html>");
   }
 }
